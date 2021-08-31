a93af143857504494b0f43c24cb735a278beb411
Daiki Ogawa <noki.g2000@gmail.com>
2021-08-31 11:28:31 +0900

    タスク一覧簡単に実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_27_063904) do
+ActiveRecord::Schema.define(version: 2021_08_31_013931) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
