ae887b7d470966f5091114a82463edbc704fe1b6
Daiki Ogawa <noki.g2000@gmail.com>
2021-09-07 14:03:12 +0900

    [fix]細かい修正

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ gem 'bootstrap'
 gem 'jquery-rails'
 
 gem 'rails-i18n'
+gem 'devise-i18n'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module TaskApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -18,7 +19,12 @@ module TaskApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    # config.i18n.default_locale = :ja
+
+    #日本語化対応
+    config.i18n.default_locale = :ja
+    # i18nの複数ロケールファイルが読み込まれるようpathを通す
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
   end
 end
