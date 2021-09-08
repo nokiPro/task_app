b21dc4b2b79808b1e25e78e8d00b44540d678f02
Daiki Ogawa <noki.g2000@gmail.com>
2021-09-08 11:44:02 +0900

    taskのviewを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :icon])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
   # アカウント編集後、プロフィール画面に移動する
